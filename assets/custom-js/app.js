3fc97db1e99bc689ad9aa3ecb3c1edf7f6c0e1a3
sachin <sachinabs.js@gmail.com>
2021-04-07 00:21:39 +0530

    updated

--- assets/custom-js/app.js
+++ assets/custom-js/app.js
@@ -96,9 +96,11 @@ function punchLine()
     function shareTwitter()
     {
     console.log(postId);
+    let quote = "I found a funny Joke for you...! Have a look...! %0D%0A"
     let shareUrl = "https://sachinabs.github.io/testJokes/show.html?postId="+postId;
     console.log(shareUrl);
-    // window.location.replace(shareUrl);
+    var tweetUrl = "https://twitter.com/intent/tweet?text="+quote+" see here %0D%0A "+shareUrl;
+    window.location.replace(tweetUrl);
     
     }
     
