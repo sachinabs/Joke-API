0267a54161770e9e946ba809bdb49ba1cddb783f
Jeya-jp <71859343+Jeya-jp@users.noreply.github.com>
2021-04-09 16:05:26 +0530

    Updated

--- assets/custom-js/app.js
+++ assets/custom-js/app.js
@@ -95,8 +95,8 @@ function punchLine()
 
     function shareTwitter()
     {
-    // console.log(postId);
-    let quote = "I found a funny Joke for you...! Have a look...! %0D%0A"
+    console.log(postId);
+    let quote = "I found a funny Joke for you...! Have a look...! %0D%0A";
     let shareUrl = "https://sachinabs.github.io/testJokes/show.html?postId="+postId;
     console.log(shareUrl);
     var tweetUrl = "https://twitter.com/intent/tweet?text="+quote+" see here %0D%0A "+shareUrl;
